5280cef2f604a9d45dfc1cf4fa1920325181ae73
ARTISANS-PLUS <bidekazima@gmail.com>
2026-07-22 14:22:30 +0000

    Update database.js

--- confi/database.js
+++ confi/database.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
 
+const connectDB = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("✅ MongoDB connecté");
+
+    }
+
+    catch(err){
+
+        console.log(err.message);
+
+        process.exit(1);
+
+    }
+
+}
+
+module.exports = connectDB;
